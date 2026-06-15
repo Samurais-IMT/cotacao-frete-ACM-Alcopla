dd8369dc01e61e6d3f6e64db9e58ba2b5e2c4051
Implantacao <implantacao.uno@gmail.com>
2026-06-15 13:32:23 -0300

    Remover tudo referente ao whats

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
 import IndexEmail from "./pages/IndexEmail.tsx";
 import NotFound from "./pages/NotFound.tsx";
 const queryClient = new QueryClient();
@@ -14,8 +13,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/email" element={<IndexEmail />} />
+          <Route path="/" element={<IndexEmail />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
